321a2b29f5f193c70a5ecfbdbfe32c0f1369b1c0
Cantero94 <jccanterogarcia@gmail.com>
2024-03-02 20:02:18 +0100

    js refine

--- 03/ficheros/script.js
+++ 03/ficheros/script.js
@@ -15,6 +15,7 @@ var papelBtn = document.getElementById("papel");
 var tijerasBtn = document.getElementById("tijeras");
 var lagartoBtn = document.getElementById("lagarto");
 var spockBtn = document.getElementById("spock");
+var hackBtn = document.getElementById("hack");
 var vsText = document.getElementById("vs");
 var usuImg = document.getElementById("img-usuario");
 var pcImg = document.getElementById("img-pc");
@@ -28,50 +29,86 @@ let computerScore = 0;
 
 var originalText = vsText.innerHTML;
 
-//PAUSAR ANIMACIÓN BOTONES CUANDO HAY MOUSEOVER Y REACTIVAR CUANDO NO
-var grupo = document.querySelectorAll('.grupo button');
-function pausarAnimacion() {
-grupo.forEach(function(boton) {
-    boton.style.animationPlayState = 'paused';
-});
+// Deshabilita los botones del juego
+function disableGameButtons() {
+    piedraBtn.disabled = true;
+    papelBtn.disabled = true;
+    tijerasBtn.disabled = true;
+    lagartoBtn.disabled = true;
+    spockBtn.disabled = true;
+    hackBtn.disabled = true;
 }
-function reanudarAnimacion() {
-    grupo.forEach(function(boton) {
-        boton.style.animationPlayState = 'running';
-    });
+
+// Habilita los botones del juego
+function enableGameButtons() {
+    piedraBtn.disabled = false;
+    papelBtn.disabled = false;
+    tijerasBtn.disabled = false;
+    lagartoBtn.disabled = false;
+    spockBtn.disabled = false;
+    hackBtn.disabled = false;
 }
-grupo.forEach(function(boton) {
-    boton.addEventListener('mouseover', pausarAnimacion);
-    boton.addEventListener('mouseout', reanudarAnimacion);
+
+// Finaliza el juego y muestra el resultado
+function endGame(winner) {
+    isPlaying = false; // Asegura que el estado del juego indique que ya no se está jugando
+    if (winner === 'user') {
+        vsText.innerHTML = "¡Has ganado el juego!";
+        vicGif.style.display = 'block';
+        document.getElementById('victmp3').play();
+    } else if (winner === 'computer') {
+        vsText.innerHTML = "Has perdido el juego.";
+        lossGif.style.display = 'block';
+        document.getElementById('lossmp3').play();
+    }
+    disableGameButtons();
+    userScore = 0;
+    computerScore = 0;
+
+    setTimeout(function() {
+        vicGif.style.display = 'none';
+        lossGif.style.display = 'none';
+        document.getElementById('victmp3').pause();
+        document.getElementById('victmp3').currentTime = 0;
+        document.getElementById('lossmp3').pause();
+        document.getElementById('lossmp3').currentTime = 0;
+        enableGameButtons();
+        userScoreSpan.textContent = userScore;
+        computerScoreSpan.textContent = computerScore;
+        vsText.innerHTML = originalText;
+    }, winner === 'user' ? 12000 : 6000);
+}
+// Evento de clic para el botón hack
+hackBtn.addEventListener("click", () => {
+    if(isPlaying) return;
+    isPlaying = true; // Evita que el juego continue después de usar el hack
+    userScore = 6; // Establece la puntuación necesaria para ganar
+    endGame('user'); // Finaliza el juego como una victoria para el usuario
 });
-//////////////////////////////////////////////////////////////////////
-//EMPIEZA EL JUEGO
-function play(usuOpcion) {
-//CAMBIA LA IMAGEN DEL JUGADOR POR LA ELECCIÓN
-    usuImg.src = "imagenes/" + usuOpcion + ".webp";
-//INICIA SONIDO MIENTRAS LA MÁQUINA ELIGE OPCIÓN
+// Función para jugar
+function play(userOption) {
+    if(isPlaying) return;
+    isPlaying = true; // Indica que el juego ha comenzado
+    disableGameButtons(); // Deshabilita los botones mientras se juega
+
+    usuImg.src = "imagenes/" + userOption + ".webp";
     document.getElementById('tic').loop = true;
     document.getElementById('tic').play();
-//CAMBIA LA IMAGEN DE MÁQUINA DURANTE LA ELECCIÓN CADA 50 MILISEGUNDOS    
-    var interval = setInterval(function(){
-        var pcOpcion = calcpcOpcion();
-        pcImg.src = "imagenes/" + pcOpcion + ".webp";
-    }, 50);    
-//CAMBIA EL VS
     vsText.innerHTML = "Eligiendo!";
-//ESTABLECE EL RETARDO ENTRE QUE ELEGIMOS LA OPCIÓN Y LA MAQUINA ELIGE LA SUYA
+    var interval = setInterval(function() {
+        var pcOption = calcpcOpcion();
+        pcImg.src = "imagenes/" + pcOption + ".webp";
+    }, 50);
+
     setTimeout(function () {
-//IGUALA LA VARIABLE pcOpcion A LA ELECCIÓN EN LA FUNCIÓN
-        var pcOpcion = calcpcOpcion();
-//UNA VEZ QUE TERMINA EL TIEMPO DE ELECCIÓN SE PARA EL SONIDO
+        clearInterval(interval);
+        var pcOption = calcpcOpcion();
         document.getElementById('tic').pause();
         document.getElementById('tic').currentTime = 0;
-        pcImg.src = "imagenes/" + pcOpcion + ".webp";
-//PARA EL CAMBIO DE IMAGENES DE LA MAQUINA
-        clearInterval(interval);
-//RECOGE EL RESULTADO DEVUELTO EN LA FUNCIÓN calcularResultado
-        var resultado = calcularResultado (usuOpcion, pcOpcion);
-        switch (resultado) {
+        pcImg.src = "imagenes/" + pcOption + ".webp";
+        var result = calcularResultado(userOption, pcOption);
+
+        switch (result) {
             case EMPATE:
                 vsText.innerHTML = "Empate!";
                 break;
@@ -84,124 +121,49 @@ function play(usuOpcion) {
                 computerScore++;
                 break;
         }
-//Actualiza en cada turno la puntuación de cada jugador
+
         userScoreSpan.textContent = userScore;
         computerScoreSpan.textContent = computerScore;
-   
-//VERIFICA SI ALGÚN JUGADOR TIENE YA MÁS DE 5 VICTORIAS Y LE DA LA PARTIDA Y BLOQUEA BOTONES, 
-//TRAS 15 SEGUNDOS PONE EN 0 LOS MARCADORES Y REHABILITA LOS BOTONES
-            if (userScore > 5) {
-                vsText.innerHTML = "¡Has ganado el juego!";
-                vicGif.style.display = 'block';
-                document.getElementById('victmp3').play();
-                disableGameButtons();
-                userScore = 0;
-                computerScore = 0;
-                setTimeout(function() {
-                vicGif.style.display = 'none';
-                enableGameButtons();
-                userScoreSpan.textContent = userScore;
-                computerScoreSpan.textContent = computerScore;
-                vsText.innerHTML = originalText;
-                }, 12000);
-            } else if (computerScore > 5) {
-                vsText.innerHTML = "Has perdido el juego.";
-                lossGif.style.display = 'block';
-                document.getElementById('lossmp3').loop = true;
-                document.getElementById('lossmp3').play();
-                disableGameButtons();
-                userScore = 0;
-                computerScore = 0;
-                setTimeout(function() {
-                lossGif.style.display = 'none';
-                document.getElementById('lossmp3').pause();
-                document.getElementById('lossmp3').currentTime = 0;
-                enableGameButtons();
-                userScoreSpan.textContent = userScore;
-                computerScoreSpan.textContent = computerScore;
-                vsText.innerHTML = originalText;
-                }, 6000);
-            }
-    }, 1000);
-    
+
+        if (userScore > 5) {
+            endGame('user');
+        } else if (computerScore > 5) {
+            endGame('computer');
+        } else {
+            isPlaying = false; // Permite iniciar un nuevo juego
+            enableGameButtons(); // Vuelve a habilitar los botones si nadie ha ganado aún
+        }
+    }, 1000); // Tiempo antes de mostrar el resultado y continuar
 }
-//ELECCIÓN DEL JUGADOR
-piedraBtn.addEventListener("click", () => {
-    play(PIEDRA);
-});
-papelBtn.addEventListener("click", () => {
-    play(PAPEL);
-});
-tijerasBtn.addEventListener("click", () => {
-    play(TIJERAS);
-});
-lagartoBtn.addEventListener("click", () => {
-    play(LAGARTO);
-});
-spockBtn.addEventListener("click", () => {
-    play(SPOCK);
-});
-//ELECCIÓN DE LA MAQUINA
+// Eventos de clic para las opciones del jugador
+piedraBtn.addEventListener("click", () => { play(PIEDRA); });
+papelBtn.addEventListener("click", () => { play(PAPEL); });
+tijerasBtn.addEventListener("click", () => { play(TIJERAS); });
+lagartoBtn.addEventListener("click", () => { play(LAGARTO); });
+spockBtn.addEventListener("click", () => { play(SPOCK); });
+
+// Genera la elección de la computadora
 function calcpcOpcion() {
     var num = Math.floor(Math.random() * 5);
     switch (num) {
-        case 0:
-            return PIEDRA;
-        case 1:
-            return PAPEL;
-        case 2:
-            return TIJERAS;
-        case 3:
-            return LAGARTO;
-        case 4:
-            return SPOCK;
+        case 0: return PIEDRA;
+        case 1: return PAPEL;
+        case 2: return TIJERAS;
+        case 3: return LAGARTO;
+        case 4: return SPOCK;
     }
 }
-//CÁLCULO DE RESULTADOS
-function calcularResultado (usuOpcion, pcOpcion) {
-    if (usuOpcion === pcOpcion) {
+// Calcula el resultado del juego
+function calcularResultado(userOption, pcOption) {
+    if (userOption === pcOption) {
         return EMPATE;
-    } else if (usuOpcion === PIEDRA) {
-        if (pcOpcion === PAPEL || pcOpcion === SPOCK)
-            return DERROTA;
-        else if (pcOpcion === TIJERAS || pcOpcion === LAGARTO)
-            return VICTORIA;
-
-    } else if (usuOpcion === PAPEL) {
-        if (pcOpcion === TIJERAS || pcOpcion === LAGARTO)
-            return DERROTA;
-        else if (pcOpcion === PIEDRA  || pcOpcion === SPOCK)
-            return VICTORIA;
-
-    } else if (usuOpcion === TIJERAS) {
-        if (pcOpcion === PIEDRA || pcOpcion === SPOCK)
-            return DERROTA;
-        else if (pcOpcion === PAPEL || pcOpcion === LAGARTO)
-            return VICTORIA;
-
-    } else if (usuOpcion === LAGARTO) {
-        if (pcOpcion === PIEDRA || pcOpcion === TIJERAS)
-            return DERROTA;
-        else if (pcOpcion === PAPEL || pcOpcion === SPOCK)
-            return VICTORIA;
-    } else if (usuOpcion === SPOCK) {
-        if (pcOpcion === PAPEL || pcOpcion === LAGARTO)
-            return DERROTA;
-        else if (pcOpcion === PIEDRA || pcOpcion === TIJERAS)
-            return VICTORIA;
+    } else if ((userOption === PIEDRA && (pcOption === TIJERAS || pcOption === LAGARTO)) ||
+               (userOption === PAPEL && (pcOption === PIEDRA || pcOption === SPOCK)) ||
+               (userOption === TIJERAS && (pcOption === PAPEL || pcOption === LAGARTO)) ||
+               (userOption === LAGARTO && (pcOption === PAPEL || pcOption === SPOCK)) ||
+               (userOption === SPOCK && (pcOption === PIEDRA || pcOption === TIJERAS))) {
+        return VICTORIA;
+    } else {
+        return DERROTA;
     }
-}
-function disableGameButtons() {
-    piedraBtn.disabled = true;
-    papelBtn.disabled = true;
-    tijerasBtn.disabled = true;
-    lagartoBtn.disabled = true;
-    spockBtn.disabled = true;
-}
-function enableGameButtons() {
-    piedraBtn.disabled = false;
-    papelBtn.disabled = false;
-    tijerasBtn.disabled = false;
-    lagartoBtn.disabled = false;
-    spockBtn.disabled = false;
 }
